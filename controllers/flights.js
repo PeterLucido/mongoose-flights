40dd9b5a60ad93fb88db4c0147841c010e745d2a
Peter Lucido <petermlucido@gmail.com>
2023-04-19 20:41:55 -0500

    solve redirecting issue

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,12 +8,9 @@ function newFlight(req, res) {
 }
 
 function create(req, res) {
-  // for (let key in req.body) {
-  //   if (req.body[key] === '') delete req.body[key]
-  // }
   Flight.create(req.body)
   .then(flight => {
-    res.redirect(`/flights/${flight._id}`)
+    res.redirect('/flights')
   })
   .catch(err => {
     console.log(err)
